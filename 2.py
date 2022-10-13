9718be2df91c46e8c1117683bafd4733c1e486c9
Bambaleiio <52800237+Bambaleiio@users.noreply.github.com>
2022-10-13 19:54:52 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,21 +1,9 @@
-import random
-
-n_1, n_2 = map(int, input().split())
+n, m = map(int, input().split())
 ans = []
-a = [int(random.random() * 1000 // 10) for i in range(n_1)]
-b = [int(random.random() * 1000 // 10) for i in range(n_2)]
+a = [int(i) for i in input().split()]
+b = [int(i) for i in input().split()]
 
 for i in a:
     if i in b and i not in ans:
         ans.append(i)
 print(ans)
-
-'''
-Идет заседание в рейстаге, присутствуют Гитлер, Гимлер, Мюллер. Заходит Штирлиц, ставит на стол поднос с апельсинами, берет из сейфа документы и уходит. Гитлер в гневе:
-- Это кто?
-Гимлер и Мюллер в один голос:
-- Это русский разведчик Исаев.
-Гитлер:
-- Так, арестуйте его!
-- Бесполезно. Скажет, что апельсины приносил...
-'''
