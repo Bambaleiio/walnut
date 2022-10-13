335dab82659b77c5a8004f68868700e1fe908a8e
Bambaleiio <52800237+Bambaleiio@users.noreply.github.com>
2022-10-13 19:56:24 +0300

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -1,23 +1,14 @@
-# вводим массив: ввод, полученный в одной строке, разбиваем на целые числа
 array = [int(i) for i in input("Enter the array: ").split()] 
-# вводим дельту
 delta = int(input("Enter the delta: "))
-
-# инициализируем переменную, в которой будем хранить минимальное значение 
-# пусть она будет равна первому элементу массива
 mmin = array[0]
 
-# проходимся по всему массиву и если mmin < array[k], то mmin = array[k] (i = array[k])
 for i in array:
     mmin = min(i, mmin)
-
-# инициализируем переменную в которой будем хранить ответ (cnt от count, ну или cunt тут хз) 
+    
 cnt = 0
 
-# если array[k] = min(array) + delta, то увеличиваем ответ на 1
 for i in array:
     if mmin + delta == i:
-        cnt += 1
-
-# выводим ответ
+        cnt += 1    
+        
 print(cnt)
